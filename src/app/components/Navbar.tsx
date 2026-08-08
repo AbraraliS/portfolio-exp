83c9acf6d54efb6af77cbf33bcc67ac3f508fece
abraralis <abrarali.sunasara28@gmail.com>
2026-08-08 12:31:53 +0530

    feat: replace logo with new red and black monogram

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import img from "@/../public/logo.svg";
+import img from "@/../public/logo.png";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
